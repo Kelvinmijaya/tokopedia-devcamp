faf5f45031651d8901126e87dd3462836a29242d
Kelvin Mijaya <kelvinmijaya@hotmail.com>
2019-02-20 01:14:32 +0700

    final code

--- src/routes/ProductDetail/index.js
+++ src/routes/ProductDetail/index.js
@@ -1,23 +1,95 @@
 import React, { Component } from 'react';
+import { get } from 'lodash';
+import { gqlFetch } from '../../helper';
 
+import productDetailQuery from './query/productDetailQuery';
 import './ProductDetail.css';
 
 class ProductDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+      loading: false,
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    // check if this different product from before
+    const slug = get(props, "match.params.slug") || "";
+    if (slug !== get(state, 'data.slug') || '') {
+
+      if (get(props, 'location.state.productData') || false) {
+        return {
+          data: props.location.state.productData,
+        };
+      } else {
+        return {
+          data: null,
+        }
+      }
+    }
+
+    return null;
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { data } = this.state;
+
+    // checking if data ready or not 
+    if(!data) {
+      this.setState({
+        loading: true,
+      })
+
+      // data not ready, fetching api 
+      gqlFetch({
+        query: productDetailQuery,
+        variables: {
+          slug: get(match, 'params.slug') || ''
+        }
+      }).then(res => {
+        this.setState({
+          data: res.getSearchDetail,
+          loading: false,
+        });
+      });
+    }
+  }
+
+  goBack = () => {
+    this.props.history.goBack();
+  }
+
   render() {
+    const { data, loading } = this.state;
+
+    // checking if still loading get api
+    if(loading) {
+      return <p>Fetching API..</p>
+    }
+
+    // product not found state 
+    if(!data && !loading) {
+      return <p>Product Not Found.</p>
+    }
+
     return <div className="productDetailWrap">
       <img
         className="productImages"
         alt="yolo"
-        src="https://peaceful-eyrie-66543.herokuapp.com/static/0.jpg"
+        src={data.imageUrl}
       />
       <div className="productInfo">
-        <h1 className="productName">Handmade Wooden Chicken</h1>
-        <span className="brandName">Mertz LLC</span>
-        <span className="productPrice">{`$ 243.00`}</span>
+        <h1 className="productName">{data.name}</h1>
+        <span className="brandName">{data.brand}</span>
+        <span className="productPrice">{`$ ${data.price}`}</span>
       </div>
       <button className="buyButton">Buy</button>
       <br/>
-      <button>Go Back</button> 
+      <button onClick={this.goBack}>Go Back</button> 
     </div>;
   }
 }
--- src/routes/Search/index.js
+++ src/routes/Search/index.js
@@ -1,13 +1,123 @@
 import React, { Component, Fragment } from "react";
+import { debounce } from "lodash";
 
+import { gqlFetch } from "../../helper";
+import { SearchContext } from "../../context/search";
+import searchQuery from "./query/searchQuery";
+import Header from "../../components/Header";
+import ProductCard from "../../components/ProductCard";
 import "./Search.css";
 
 class Search extends Component {
+  static contextType = SearchContext;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchWord: props.query
+    };
+  }
+
+  componentDidMount() {
+    const { updateSearch, list, keyword } = this.context;
+
+    // only fetching when list empty
+    if (list.length === 0) {
+      gqlFetch({
+        query: searchQuery,
+        variables: {
+          page: 1,
+          perPage: 10,
+          q: keyword
+        }
+      }).then(res => {
+        if (res.getSearch) {
+          updateSearch({
+            productData: res.getSearch.products,
+            keywordData: keyword,
+            hasNextPage: res.getSearch.hasNextPage
+          });
+        }
+      });
+    }
+  }
+
+  // function for load more products
+  loadMore = () => {
+    const { updateSearch, keyword, page } = this.context;
+
+    gqlFetch({
+      query: searchQuery,
+      variables: {
+        page: page + 1,
+        perPage: 10,
+        q: keyword
+      }
+    }).then(res => {
+      if (res.getSearch) {
+        updateSearch({
+          productData: res.getSearch.products,
+          keywordData: keyword,
+          hasNextPage: res.getSearch.hasNextPage
+        });
+      }
+    });
+  }
+
+  // change keyword search
+  changeWord = e => {
+    this.setState(
+      {
+        searchWord: e.target.value
+      },
+      () => {
+        this.debounceSearchQuery(this.state.searchWord);
+      }
+    );
+  };
+
+  // debounce 500ms for each typing
+  debounceSearchQuery = debounce(searchWord => {
+    const { updateSearch } = this.context;
+
+    gqlFetch({
+      query: searchQuery,
+      variables: {
+        page: 1,
+        perPage: 10,
+        q: searchWord
+      }
+    }).then(res => {
+      updateSearch({
+        productData: res.getSearch.products,
+        keywordData: searchWord,
+        hasNextPage: res.getSearch.hasNextPage
+      });
+    });
+  }, 500);
+
   render() {
+    const { list, hasNextPage } = this.context;
+    const { searchWord } = this.state;
+
     return (
       <Fragment>
+        <Header changeWord={this.changeWord} searchWord={searchWord} />
         <div className="searchWrapper">
-        <p>Welcome to DevCamp Tokopedia</p>
+          {list.map((item, index) => (
+            <ProductCard
+              key={`product-${index}`}
+              imageUrl={item.imageUrl}
+              id={item.id}
+              name={item.name}
+              brand={item.brand}
+              price={item.price}
+              slug={item.slug}
+            />
+          ))}
+
+          {hasNextPage && <button className="loadmoreButton" onClick={this.loadMore}>Load More</button>}
         </div>
       </Fragment>
     );
